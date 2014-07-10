417110aa08f589e099c6acff19e0c856300420fd
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-07-10 18:57:42 +0800

    Added spread-namespace.js file.

---
+++ spread-namespace.js
@@ -0,0 +1,23 @@
+/*:
+	@module-configuration:
+		{
+			"packageName": "spread-namespace",
+			"fileName": "spread-namespace.js",
+			"moduleName": "spreadNamespace",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com:volkovasystems/spread-namespace.git",
+			"testCase": "spread-namespace-test.js",
+			"isGlobal": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+
+	@end-module-documentation
+*/
+var spreadNamespace = function spreadNamespace( namespace ){
+
+};
+
+( module || { } ).exports = spreadNamespace;
